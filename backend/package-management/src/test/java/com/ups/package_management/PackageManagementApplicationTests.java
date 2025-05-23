92f906fd95a590e67dbb501fcbcca4e365879b58
Gomathi Manisha <gomathimanishaa@gmail.com>
2025-05-23 20:15:27 +0530

    Initialize Spring Boot backend and Next.js frontend

---
+++ backend/package-management/src/test/java/com/ups/package_management/PackageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ups.package_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
